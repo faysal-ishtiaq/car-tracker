5382a79683f22ed84acdfb08e1a41ad98e5b2908
Faysal Ishtiaq <f.i.rabby@gmail.com>
2021-01-21 22:24:24 +0600

    write python 3.5 compatible code

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -2,9 +2,9 @@ import os
 
 
 def get_data_path(filename: str) -> str:
-    return f"/home/{os.getlogin()}/car-tracker/data/{filename}"
+    return f"/home/%s/car-tracker/data/%s".format(os.getlogin(), filename)
 
 
 def get_yolo_path(filename: str) -> str:
-    return f"/home/{os.getlogin()}/car-tracker/yolov4/{filename}"
+    return f"/home/%s/car-tracker/yolov4/%s".format(os.getlogin(), filename)
 
