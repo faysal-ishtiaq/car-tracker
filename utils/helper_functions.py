48a6f402dc44de5a6c364f0408e096d15922ae78
Faysal Ishtiaq <f.i.rabby@gmail.com>
2021-01-21 23:25:09 +0600

    support pythonv3.5

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,10 +1,10 @@
-import os
+import getpass
 
 
 def get_data_path(filename: str) -> str:
-    return '/home/{}/car-tracker/data/{}'.format(os.getlogin(), filename)
+    return '/home/{}/car-tracker/data/{}'.format(getpass.getuser(), filename)
 
 
 def get_yolo_path(filename: str) -> str:
-    return '/home/{}/car-tracker/yolov4/{}'.format(os.getlogin(), filename)
+    return '/home/{}/car-tracker/yolov4/{}'.format(getpass.getuser(), filename)
 
